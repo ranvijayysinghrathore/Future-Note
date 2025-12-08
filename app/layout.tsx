e0f794ad25f7dce3d573c13527cd3c8251deab17
ChanchalSinghRathore <chanchalrathore111003@gmail.com>
2025-12-09 01:21:09 +0530

    feat: integrate vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react"
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
@@ -22,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
